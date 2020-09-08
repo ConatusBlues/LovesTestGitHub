efddad66c29081b457a7703db8abd4198d641fe9
Love Lindqvist <lindqvistlove@hotmail.com>
2020-09-08 20:55:23 +0200

    testar en klass

---
+++ LovesTestGitHub/TestKlass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LovesTestGitHub
+{
+    class TestKlass
+    {
+        public int MyProperty { get; set; }
+    }
+}
